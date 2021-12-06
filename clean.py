79e09deedfbb0317f2ca16f26137c7eeea6ecbe3
evankomp@uw.edu <evankomp@uw.edu>
2021-12-06 11:06:48 -0800

    drop old index from column

--- clean.py
+++ clean.py
@@ -19,7 +19,7 @@ def clean_raw_csv(df):
         ]
     ]
     df_.rename(columns={'BEGIN_LAT': 'LATITUDE', 'BEGIN_LON': 'LONGITUDE'}, inplace=True)
-    df_.reset_index(inplace=True)
+    df_.reset_index(inplace=True, drop=True)
     return df_
 
 df_ = clean_raw_csv(df)
